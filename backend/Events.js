31d5e89d276e4ca9801b37057a8cfbc23914e979
Viktor Zhurbin <vzhurbin@gmail.com>
2018-01-25 10:07:04 +0300

    add backend

---
+++ backend/Events.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'TIFF',
+        description: 'Toronto International Film Festival',
+        date: '2018-09-10'
+    },
+    {
+        id: 2,
+        name: 'The Martian Premiere',
+        description: 'The Martian comes to theatres.',
+        date: '2018-10-02'
+    },
+    {
+        id: 3,
+        name: 'SXSW',
+        description: 'Music, film and interactive festival in Austin, TX.',
+        date: '2018-03-11'
+    }
+];
---
+++ backend/api.js
@@ -0,0 +1,9 @@
+const Events = require('./Events.js');
+
+exports.events = function (req, res) {
+  res.json(Events);
+};
+
+exports.event = function (req, res) {
+  res.json(Events[req.param.eventId]);
+};
