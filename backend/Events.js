9b6213a9eb6b78fe84ae23842b0fe355f2c0c8fd
Viktor Zhurbin <vzhurbin@gmail.com>
2018-01-25 10:11:26 +0300

    add dependencies

--- backend/Events.js
+++ backend/Events.js
@@ -2,13 +2,13 @@ module.exports = [
     {
         id: 1,
         name: 'TIFF',
-        description: 'Toronto International Film Festival',
+        description: 'Moscow International Film Festival',
         date: '2018-09-10'
     },
     {
         id: 2,
-        name: 'The Martian Premiere',
-        description: 'The Martian comes to theatres.',
+        name: 'The Viktor Premiere',
+        description: 'Viktor comes to theatres.',
         date: '2018-10-02'
     },
     {
